9d3b6765a0364e0850455e466826bc755fbc8a52
Cedric <25cedliu@student.cfschools.org>
2025-01-23 18:55:37 -0600

    temp, wip

--- EV_Code_24.ino
+++ EV_Code_24.ino
@@ -3,12 +3,14 @@
 
 // Motor Control Pins
 int motor_c_ENA = 9;  // Speed Control
+int motor_c_ENB = 10;  
 int motor_c_IN1 = 6; // Direction Control
 int motor_c_IN2 = 7;
 
 // Encoder Pins
 #define ENCODER_A_PIN 2
 #define ENCODER_B_PIN 3
+#define STBY 8
 
 const long target_distance_pulses = 3057; 
 long pulse_count = 0; 
@@ -19,8 +21,13 @@ int min_speed = 100;
 int slow_down_threshold = 1000; 
 
 void setup() {
+    // Motor Standby Settings
+    pinMode(STBY, OUTPUT)
+    digitalWrite(STBY, 1)
+
     // Motor Pin Setup
     pinMode(motor_c_ENA, OUTPUT);
+    pinMode(motor_c_ENB, OUTPUT);
     pinMode(motor_c_IN1, OUTPUT);
     pinMode(motor_c_IN2, OUTPUT);
 
@@ -38,6 +45,7 @@ void loop() {
 
     // Dynamically adjust speed based on remaining distance
     int current_speed = max_speed;
+
     if (remaining_pulses <= slow_down_threshold) {
         current_speed = min_speed + (remaining_pulses * (max_speed - min_speed)) / slow_down_threshold;
         if (current_speed > max_speed) {
@@ -77,4 +85,40 @@ void stopMotor() {
     analogWrite(motor_c_ENA, 0);
     digitalWrite(motor_c_IN1, LOW);
     digitalWrite(motor_c_IN2, LOW);
+}
+
+
+
+
+
+
+
+
+const int BUTTON_PIN = 7;
+int lastState = HIGH;
+int currentState == LOW;
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop() 
+{
+  lastState = currentState;
+  currentState = digitalRead(BUTTON_PIN);
+
+  if (lastState == HIGH && currentState == LOW)
+  {
+    run();
+    return;
+  }
+}
+
+//Looped code for moving car! (Can be anything we want to run though)
+void run()
+{
+  //Driving code
 }
